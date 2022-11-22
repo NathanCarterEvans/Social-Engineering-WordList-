f0f9c7c0938fbe5e339c4ce205f4514a4b8cad63
Nathan <natec.carter15@gmail.com>
2022-11-21 19:09:49 -0600

    updated the Facebook scrape to find name, education, and class date.

--- scrape.py
+++ scrape.py
@@ -1,7 +1,46 @@
 from facebook_scraper import get_posts, get_profile
+import sys
 
-item = get_profile("")
+args = sys.argv
+name = []
+education = []
+dates = []
+
+'''
+    --help      -h: display this help message
+    --facebook  -f: facebook {-f [profilename]}
 
 
-with open("dump.txt","w") as file:
-    file.write(f"name: {item['Name']}\n")
+
+'''
+
+def scanFacebook():
+    global args, name, dates
+
+    if "-f" in args:
+        profile = args.index("-f")
+    else:
+        profile = args.index("--find")
+
+    info = get_profile(args[profile+1])
+
+    name.append(info["Name"])
+    sentence = ""
+    for i in info["Education"]:
+        if(i != "\n"):
+            sentence += i
+        else:
+            education.append(sentence)
+            sentence = ""
+    
+    for i in education:
+        if("Class of" in i):
+            dates.append(i[-2:])
+
+
+
+#main 
+print(args)
+if('-f' in args or '--facebook' in args):
+    print("MADE IT")
+    scanFacebook()
