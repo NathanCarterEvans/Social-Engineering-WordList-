6ef7f6fd729a5c61cc9f0bfd85a447a0e81bf6f2
Nathan <natec.carter15@gmail.com>
2022-11-24 14:26:49 -0600

    Added function to write to file

--- scrape.py
+++ scrape.py
@@ -17,8 +17,9 @@ tiktok
 
 
 '''
+    use:    python3 scrape.py [settings] 
     --help      -h: display this help message
-    --facebook  -f: facebook {-f [profilename]}
+    --facebook  -f: facebook {-f [link to profile]}
 
 
 
@@ -32,25 +33,63 @@ def scanFacebook():
     else:
         profile = args.index("--find")
 
-    info = get_profile(args[profile+1])
-
-    name.append(info["Name"])
-    sentence = ""
-    for i in info["Education"]:
-        if(i != "\n"):
-            sentence += i
-        else:
-            education.append(sentence)
-            sentence = ""
+    profileName = args[profile+1]
+    for i in range(len(profileName)):
+        print(profileName[i:i+4])
+        if(profileName[i:i+4] == "com/"):
+            profileName = profileName[i+4:]
+            print(profileName)
+            
     
-    for i in education:
-        if("Class of" in i):
-            dates.append(i[-2:])
+
+    info = get_profile(profileName)
+    # print(info)
+
+    #find Name
+    try:
+        name.append(info["Name"])
+    except KeyError:
+        print("No Name Found on FaceBook")
+
+    #find Education
+    try:
+        sentence = ""
+        for i in info["Education:"]:
+            if(i != "\n"):
+                sentence += i
+            else:
+                education.append(sentence)
+                sentence = ""
+    
+        for i in education:
+            if("Class of" in i):
+                dates.append(f"Graduation Date: {i[-4:]}")
+    except KeyError:
+        print("No Education Found on FaceBook")
+        
+
+
+def fileWrite():
+    with open("InfoDump.txt","w") as file:
+        file.write("Name:\n")
+        for i in name:
+            file.write(f"\t{i}\n")
+        
+        file.write("\nEducation\n")
+        for i in education:
+            file.write(f"\t{i}\n")
+        
+        file.write("\nDates:\n")
+        for i in dates:
+            file.write(f"\t{i}\n")
+
+
 
 
 
 #main 
-print(args)
+# print(args)
 if('-f' in args or '--facebook' in args):
-    print("MADE IT")
+    # print("MADE IT")
     scanFacebook()
+fileWrite()
